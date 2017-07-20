cd5e1f6a1c010c536bc568846a2aecb7647e0605
dylan-zwl <294598356@qq.com>
2017-07-20 18:55:45 +0800

    ss

--- app/src/main/java/com/tapc/update/ui/fragment/install/InstallAppFragment.java
+++ app/src/main/java/com/tapc/update/ui/fragment/install/InstallAppFragment.java
@@ -1,17 +1,12 @@
 package com.tapc.update.ui.fragment.install;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageInstallObserver;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.os.Handler;
 import android.os.RemoteException;
-import android.support.v4.app.Fragment;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -24,8 +19,10 @@ import com.tapc.update.R;
 import com.tapc.update.application.Config;
 import com.tapc.update.ui.adpater.BaseAppAdpater;
 import com.tapc.update.ui.entity.AppInfoEntity;
+import com.tapc.update.ui.fragment.BaseFragment;
 import com.tapc.update.ui.view.CustomTextView;
 import com.tapc.update.utils.AppUtil;
+import com.tapc.update.utils.ShowInforUtil;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -35,122 +32,65 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class InstallAppFragment extends Fragment {
+public class InstallAppFragment extends BaseFragment {
     @BindView(R.id.install_app_lv)
     ListView mInstallAppLv;
     @BindView(R.id.install_all_chk)
     CheckBox mAllCheck;
 
-    private Context mContext;
     private Handler mHandler;
     private BaseAppAdpater mAdapter;
     private List<String> mListFilePath = new ArrayList<String>();
     private List<AppInfoEntity> mListApkInfor = new ArrayList<AppInfoEntity>();
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_install_app, container, false);
-        ButterKnife.bind(this, view);
-        initView();
-        return view;
+    public int getContentView() {
+        return R.layout.fragment_install_app;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        mContext = activity;
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-    }
-
-    private void initView() {
+    public void initView() {
         mHandler = new Handler();
-        getFiles(Config.INSTALL_APP_FILE_PATH, ".apk", true);
-        if (mListFilePath != null && mListFilePath.size() > 0) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String path = Config.MOUNTED_PATH + Config.SAVEFILE_PATH + Config.INSTALL_APP_PATH;
+                getFiles(path, ".apk");
+                if (mListFilePath != null && mListFilePath.size() > 0) {
                     getAppList();
+                    mAdapter = new BaseAppAdpater(mListApkInfor, new BaseAppAdpater.Listener() {
+                        @Override
+                        public View getView(int position, View convertView, ViewGroup parent) {
+                            return initItemView(position, convertView);
+                        }
+                    });
                     mHandler.post(new Runnable() {
                         @Override
                         public void run() {
-                            mAdapter = new BaseAppAdpater(mListApkInfor, new BaseAppAdpater.Listener() {
-                                @Override
-                                public View getView(int position, View convertView, ViewGroup parent) {
-                                    return initItemView(position, convertView);
-                                }
-                            });
                             mInstallAppLv.setAdapter(mAdapter);
-                            mAdapter.notifyDataSetChanged();
+                            notifyChanged();
                         }
                     });
                 }
-            }).start();
-        }
+            }
+        }).start();
+
         mAllCheck.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                for (int index = 0; index < mListApkInfor.size(); index++) {
-                    mListApkInfor.get(index).setChecked(isChecked);
+                if (mListApkInfor != null) {
+                    for (int index = 0; index < mListApkInfor.size(); index++) {
+                        mListApkInfor.get(index).setChecked(isChecked);
+                    }
                     notifyChanged();
                 }
             }
         });
     }
 
-    class ViewHolder {
-        @BindView(R.id.install_chk)
-        CheckBox checkBox;
-        @BindView(R.id.install_app_ic)
-        ImageView icon;
-        @BindView(R.id.install_app_name)
-        CustomTextView name;
-        @BindView(R.id.install_app_status)
-        CustomTextView installStatus;
-        @BindView(R.id.install_start_btn)
-        Button start;
-    }
-
-    private View initItemView(final int position, View convertView) {
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            convertView = View.inflate(mContext, R.layout.item_install_app, null);
-            viewHolder = new ViewHolder();
-            ButterKnife.bind(viewHolder, convertView);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-        final AppInfoEntity item = mListApkInfor.get(position);
-        viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                mListApkInfor.get(position).setChecked(isChecked);
-            }
-        });
-        viewHolder.checkBox.setChecked(item.isChecked());
-        viewHolder.icon.setImageDrawable(item.getAppIcon());
-        viewHolder.name.setText(item.getAppLabel());
-        viewHolder.installStatus.setText(item.getInstallStatus());
-        viewHolder.start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                installApp(position, false);
-            }
-        });
-        return convertView;
-    }
-
     // 搜索目录，扩展名，是否进入子文件夹
-    private void getFiles(String Path, String Extension, boolean IsIterative) {
+    private void getFiles(String Path, String Extension) {
         File[] files = new File(Path).listFiles();
         if (files != null) {
             for (int i = 0; i < files.length; i++) {
@@ -193,35 +133,65 @@ public class InstallAppFragment extends Fragment {
             for (int index = 0; index < mListApkInfor.size(); index++) {
                 installApp(index, true);
             }
+            startUpdate();
         }
     };
 
-    private void installApp(int index, boolean isNeedChecked) {
+    private void installApp(final int index, boolean isNeedChecked) {
         AppInfoEntity appInfoEntity = mListApkInfor.get(index);
         if (isNeedChecked && appInfoEntity.isChecked() == false) {
             return;
         }
         appInfoEntity.setInstallStatus("");
         notifyChanged();
-
-        final int listIndex = index;
+        incTask();
         String path = appInfoEntity.getPath();
         boolean result = AppUtil.installApk(mContext, new File(path), new IPackageInstallObserver.Stub() {
             @Override
             public void packageInstalled(String s, int i) throws RemoteException {
-                if (i == 0) {
-                    mListApkInfor.get(listIndex).setInstallStatus(getResources().getString(R.string
+                boolean isSuccess;
+                if (i == 1) {
+                    mListApkInfor.get(index).setInstallStatus(getResources().getString(R.string
                             .app_install_success));
+                    isSuccess = true;
                 } else {
-                    mListApkInfor.get(listIndex).setInstallStatus(getResources().getString(R.string
+                    mListApkInfor.get(index).setInstallStatus(getResources().getString(R.string
                             .app_install_fail));
+                    isSuccess = false;
+
                 }
+                ShowInforUtil.send(mContext, mListApkInfor.get(index).getAppLabel(), getString(R.string.uninstall),
+                        isSuccess, "");
+
+                decTask();
+                stopUpdate();
                 notifyChanged();
             }
         });
         if (result == false) {
-            mListApkInfor.get(listIndex).setInstallStatus(getResources().getString(R.string.app_install_fail));
+            mListApkInfor.get(index).setInstallStatus(getResources().getString(R.string.app_install_fail));
+            decTask();
+            stopUpdate();
         }
+    }
+
+    @OnClick(R.id.install_all_app_btn)
+    void installAllApp(View v) {
+        new Thread(mInstallAppRunnable).start();
+        startUpdate();
+    }
+
+    class ViewHolder {
+        @BindView(R.id.install_chk)
+        CheckBox checkBox;
+        @BindView(R.id.install_app_ic)
+        ImageView icon;
+        @BindView(R.id.install_app_name)
+        CustomTextView name;
+        @BindView(R.id.install_app_status)
+        CustomTextView installStatus;
+        @BindView(R.id.install_start_btn)
+        Button start;
     }
 
     private void notifyChanged() {
@@ -233,8 +203,36 @@ public class InstallAppFragment extends Fragment {
         });
     }
 
-    @OnClick(R.id.install_all_app_btn)
-    void installAllApp(View v) {
-        new Thread(mInstallAppRunnable).start();
+    private View initItemView(final int position, View convertView) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.item_install_app, null);
+            viewHolder = new ViewHolder();
+            ButterKnife.bind(viewHolder, convertView);
+            convertView.setTag(viewHolder);
+
+            viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    mListApkInfor.get(position).setChecked(isChecked);
+                }
+            });
+
+            viewHolder.start.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    installApp(position, false);
+                    startUpdate();
+                }
+            });
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        final AppInfoEntity item = mListApkInfor.get(position);
+        viewHolder.checkBox.setChecked(item.isChecked());
+        viewHolder.icon.setImageDrawable(item.getAppIcon());
+        viewHolder.name.setText(item.getAppLabel());
+        viewHolder.installStatus.setText(item.getInstallStatus());
+        return convertView;
     }
 }
